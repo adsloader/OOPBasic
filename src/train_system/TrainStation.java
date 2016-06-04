e017a168e6a279601e3ae110ce37a40afd0b9dba
snake <adsloader99@gmail.com>
2016-06-04 19:39:17 +0900

    한글

--- src/train_system/TrainStation.java
+++ src/train_system/TrainStation.java
@@ -2,24 +2,24 @@ package  train_system;
 
 public class TrainStation {
     public static void main(String [] args){
-    	FastTrain train = new FastTrain();
+    	KTX열차 train = new KTX열차();
     	
     	// 태우고 이동
-    	Passenger person1 = new Passenger("박모씨", "부산");
-    	Passenger person2 = new Passenger("김모씨", "천안");
+    	여행객 person1 = new 여행객("박모씨", "부산");
+    	여행객 person2 = new 여행객("김모씨", "천안");
     	train.passenger_in_train(person1);
     	train.passenger_in_train(person2);
     	train.GoNextStation();
     	
     	// 태우고 이동
-    	Passenger person3 = new Passenger("한모씨", "부산");
+    	여행객 person3 = new 여행객("한모씨", "부산");
     	train.passenger_in_train(person3);
     	
     	// 이동
     	train.GoNextStation();
     	train.GoNextStation();
     	train.GoNextStation();
-    	train.GoNextStation();
-    	
-    }
+
+		train.GoNextStation();
+	}
 }
